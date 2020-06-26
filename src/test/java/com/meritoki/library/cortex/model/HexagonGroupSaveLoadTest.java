7a81b91da19196d78cbe7a146529e780e3d3af09
meritkoi <meritoki@yahoo.com>
2020-06-26 12:34:38 -0300

    Adding unit tests

---
+++ src/test/java/com/meritoki/library/cortex/model/HexagonGroupSaveLoadTest.java
@@ -0,0 +1,29 @@
+package com.meritoki.library.cortex.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class HexagonGroupSaveLoadTest {
+
+	@BeforeAll
+	public static void initialize() {
+		
+	}
+	
+	@Test
+	@Order(1)
+	public void save() {
+		
+	}
+	
+	@Test
+	@Order(2)
+	public void load() {
+		
+	}
+	
+}
